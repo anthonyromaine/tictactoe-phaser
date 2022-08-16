34e514458f54e3a226cc35bc26b8895073091410
Anthony Romaine <anthony.romaine1@gmail.com>
2022-08-16 01:09:20 +0000

    Add home and replay buttons to GameOver scene

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -12,10 +12,39 @@ export default class GameOver extends Phaser.Scene {
     console.log(data.player);
   }
 
+  preload() {
+    this.load.image('home', '/src/assets/home.png');
+    this.load.image('replay', '/src/assets/replay.png');
+  }
+
   create() {
     const width = this.scale.width;
     const height = this.scale.height;
 
+    // add home button
+    let homeBtn = this.add.image(width * 0.7, height * 0.7, 'home').setScale(0.6);
+    homeBtn.setInteractive();
 
+    // add home button
+    let replayBtn = this.add.image(width * 0.3, height * 0.7, 'replay').setScale(0.6);
+    replayBtn.setInteractive();
+
+    this.input.on('gameobjectdown', this.handleClick);
+  }
+
+  handleClick(pointer, object) {
+    if (object.texture.key === 'home') {
+      this.scene.handleHome();
+    } else if (object.texture.key === 'replay') {
+      this.scene.handleReplay();
+    }
+  }
+
+  handleHome() {
+    this.scene.start('home');
+  }
+
+  handleReplay() {
+    this.scene.start('game');
   }
 }
