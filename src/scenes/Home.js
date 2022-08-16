4200f6e7e25262305456b5c9480b677eb9be9291
Anthony Romaine <anthony.romaine1@gmail.com>
2022-08-16 00:28:16 +0000

    Add logo and play button to Home

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -9,6 +9,11 @@ export default class Home extends Phaser.Scene {
 
   }
 
+  preload() {
+    this.load.image('logo', '/src/assets/logo.png');
+    this.load.image('play', '/src/assets/play.png');
+  }
+
   create() {
     // set background color
     this.mainCamera = this.cameras.add(0, 0);
@@ -16,6 +21,12 @@ export default class Home extends Phaser.Scene {
 
     const width = this.scale.width;
     const height = this.scale.height;
+
+    // add logo
+    this.add.image(width * 0.5, height * 0.4, 'logo').setScale(0.5);
+
+    // add play button
+    this.add.image(width * 0.5, height * 0.7, 'play').setScale(0.6);
 
 
   }
