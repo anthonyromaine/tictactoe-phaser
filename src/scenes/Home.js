21bef1df2d1971ba740927151cec987d30f476ad
Anthony Romaine <anthony.romaine1@gmail.com>
2022-08-17 02:03:00 +0000

    Implement fullscreen on home scene

--- src/scenes/Home.js
+++ src/scenes/Home.js
@@ -5,6 +5,8 @@ export default class Home extends Phaser.Scene {
     super('home');
   }
 
+  fullscreenAvailable = false;
+
   init() {
     // set background color
     this.mainCamera = this.cameras.add(0, 0);
@@ -14,11 +16,27 @@ export default class Home extends Phaser.Scene {
   preload() {
     this.load.image('logo', '/src/assets/logo.png');
     this.load.image('play', '/src/assets/play.png');
+    this.load.image('larger', '/src/assets/larger.png');
+    this.load.image('smaller', '/src/assets/smaller.png');
   }
 
   create() {
     const width = this.scale.width;
     const height = this.scale.height;
+
+    this.fullscreenAvailable = this.sys.game.device.fullscreen.available;
+
+    if (this.fullscreenAvailable) {
+      let fullscreenBtn;
+      if (this.scale.isFullscreen) {
+        fullscreenBtn = this.add.image(width * 0.9, height * 0.94, 'smaller').setScale(0.6);
+      } else {
+        fullscreenBtn = this.add.image(width * 0.9, height * 0.94, 'larger').setScale(0.6);
+      }
+      fullscreenBtn.setInteractive();
+
+      // toggleFullscreen
+    }
 
     // add logo
     this.add.image(width * 0.5, height * 0.4, 'logo').setScale(0.5);
@@ -31,12 +49,32 @@ export default class Home extends Phaser.Scene {
 
   }
 
-  handleClick() {
-    this.scene.startGame();
+  handleClick(pointer, object) {
+    if (object.texture.key === 'play') {
+      this.scene.startGame();
+    } else if (this.scene.isFullscreenBtn(object.texture.key)) {
+      this.scene.handleFullScreen(object);
+    }
+  }
+
+  isFullscreenBtn(key) {
+    if (key === 'smaller' || key === 'larger') {
+      return true;
+    }
+    return false;
   }
 
   startGame() {
     this.scene.start('game');
+  }
+
+  handleFullScreen(object) {
+    this.scale.toggleFullscreen();
+    if (object.texture.key === 'smaller') {
+      object.setTexture('larger');
+    } else {
+      object.setTexture('smaller');
+    }
   }
 
 }
